6cd23fe4674c45682970e07f86ab59b73f9f3e29
Hitesh Pachpor <hiteshpachpor@gmail.com>
2018-12-07 18:00:44 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta property="og:title" content="Hitesh weds Ipsita - 31st December 2018"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="http://0cea1e8c.ngrok.io/hiteshchiipsita"/>
+    <meta property="og:image" content="http://0cea1e8c.ngrok.io/hiteshchiipsita/assets/images/5s.jpg"/>
+    <meta property="og:site_name" content="0cea1e8c.ngrok.io"/>
+    <meta property="og:description" content="We invite you to our wedding ceremony & reception on Monday, 31st December @ De Grandeur Banquets, Thane West."/>
+    <meta name="description" content="We invite you to our wedding ceremony & reception on Monday, 31st December @ De Grandeur Banquets, Thane West."/>
+
+    <!-- Desktop browsers (including 16x6, 32x32 & 48x48 PNGs) -->
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <!-- Modern browsers (196x196 should cover all modern browsers) -->
+    <link rel="icon" type="image/png" href="favicon.png">
+
+    <!-- iOS & other mobile devices (ideally 180x180, at least 120x120) -->
+    <link rel="apple-touch-icon" href="touch-icon-iphone.png">
+
+    <title>Hitesh Chi Ipsita - 31st December 2018</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="assets/css/style.css?t=<?php echo time(); ?>">
+
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:200,300,400,400i,500,600|Gamja+Flower|Karla|Slabo+27px|Bad+Script" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <a href="" class="h-i text-center">
+            <span class="h">H</span>
+            <span class="and">&</span>
+            <span class="i">I</span>
+        </a>
+
+        <hr>
+
+        <h1 class="main-heading text-center">
+            Celebrate With Us
+        </h1>
+
+        <h2 class="sub-heading text-center">December 31, 2018</h2>
+
+        <div class="row main-banner">
+            <div class="col-12">
+                <img class="img-fluid rounded" src="assets/images/14.jpg" alt="Hitesh and Ipsita candid">
+            </div>
+        </div>
+
+        <h2 class="text-center font-italic hashtag">#HiteshChiIpsy</h2>
+
+        <p class="generic-text">It very rarely happens that two people on a first date realize that they're looking at their future. And we were lucky enough to experience this.</p>
+
+        <p class="generic-text">In a very short span of time, we have got to know & understand each other as good as two people who have been together for years.</p>
+
+        <p class="generic-text">And now we are taking it to the next level by tying the knot!</p>
+
+        <div class="row align-items-center relative visit-our-memory-lane-container">
+            <div class="col-4">
+                <img class="img-fluid rounded" src="assets/images/first_date.jpg" alt="Hitesh and Ipsita candid">
+            </div>
+            <div class="col-4 no-padding-right no-padding-left">
+                <img class="img-fluid rounded" src="assets/images/13s.jpg" alt="Hitesh and Ipsita candid">
+            </div>
+            <div class="col-4">
+                <img class="img-fluid rounded" src="assets/images/4060.jpg" alt="Hitesh and Ipsita candid">
+            </div>
+            <div class="opaque"></div>
+            <a href="memory-lane" class="btn btn-primary memory-lane-button">Visit Our Memory Lane</a>
+        </div>
+
+        <p class="generic-text"></p>
+
+        <p class="generic-text">The end of this year marks the beginning of an exciting new chapter in our lives.</p>
+
+        <p class="generic-text">We, Hitesh & Ipsita, request the honour of your presence at our wedding ceremony on Monday, the 31st of December, 2018.</p>
+
+        <p class="generic-text">
+            <span class="font-weight-bold">Muhurta:</span> 11:21 AM
+            <br>
+            <span class="font-weight-bold">Lunch:</span> 12 PM onwards
+            <br>
+            <span class="font-weight-bold">Reception:</span> 12:30 PM onwards
+        </p>
+
+        <p class="generic-text">
+            <span class="font-weight-bold">Venue:</span>
+            <br>
+            <span class="font-weight-bold">De Grandeur Hotel And Banquets</span>
+            <br>
+            <span>Anand Nagar, Near Bhakti Park, Ghodbunder Road, Thane West.</span>
+        </p>
+
+        <div>
+            <a class="btn btn-outline-primary map-link" href="https://goo.gl/maps/69ywYt1u3NG2" target="_blank">Open in Maps &#10140;</a>
+        </div>
+
+        <div class="we-are-waiting">
+            <div class="row">
+                <div class="img-container">
+                    <img src="assets/images/17.jpg" alt="We are waiting" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ memory-lane/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta property="og:title" content="Hitesh weds Ipsita - 31st December 2018"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="http://0cea1e8c.ngrok.io/hiteshchiipsita"/>
+    <meta property="og:image" content="http://0cea1e8c.ngrok.io/hiteshchiipsita/assets/images/5s.jpg"/>
+    <meta property="og:site_name" content="0cea1e8c.ngrok.io"/>
+    <meta property="og:description" content="We invite you to our wedding ceremony & reception on Monday, 31st December @ De Grandeur Banquets, Thane West."/>
+    <meta name="description" content="We invite you to our wedding ceremony & reception on Monday, 31st December @ De Grandeur Banquets, Thane West."/>
+
+    <!-- Desktop browsers (including 16x6, 32x32 & 48x48 PNGs) -->
+    <link rel="shortcut icon" href="../favicon.ico">
+
+    <!-- Modern browsers (196x196 should cover all modern browsers) -->
+    <link rel="icon" type="image/png" href="../favicon.png">
+
+    <!-- iOS & other mobile devices (ideally 180x180, at least 120x120) -->
+    <link rel="apple-touch-icon" href="../touch-icon-iphone.png">
+
+    <title>Hitesh Chi Ipsita - 31st December 2018</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../assets/css/style.css?t=<?php echo time(); ?>">
+
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:200,300,400,400i,500,600|Gamja+Flower|Karla|Slabo+27px|Bad+Script" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/solid.css" integrity="sha384-rdyFrfAIC05c5ph7BKz3l5NG5yEottvO/DQ0dCrwD8gzeQDjYBHNr1ucUpQuljos" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/regular.css" integrity="sha384-z3ccjLyn+akM2DtvRQCXJwvT5bGZsspS4uptQKNXNg778nyzvdMqiGcqHVGiAUyY" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/fontawesome.css" integrity="sha384-u5J7JghGz0qUrmEsWzBQkfvc8nK3fUT7DCaQzNQ+q4oEXhGSx+P2OqjWsfIRB8QT" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <a href="../" class="h-i text-center">
+            <span class="h">H</span>
+            <span class="and">&</span>
+            <span class="i">I</span>
+        </a>
+
+        <hr>
+
+        <h1 class="main-heading text-center">
+            Memory Lane
+        </h1>
+
+        <!-- <h2 class="sub-heading text-center">&nbsp;</h2> -->
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><i class="fas fa-camera-retro"></i><span class="font-weight-bold">Our first date</span></div>
+                <img class="img-fluid rounded" src="../assets/images/first_date.jpg" alt="Hitesh and Ipsita candid">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><i class="fas fa-camera-retro"></i><span class="font-weight-bold">Guess who cracks the best jokes</span></div>
+                <img class="img-fluid rounded" src="../assets/images/bad_joke.jpeg" alt="Hitesh puns and Ipsita grumps">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><i class="fas fa-camera-retro"></i><span class="font-weight-bold">Happy ducks</span></div>
+                <img class="img-fluid rounded" src="../assets/images/cheesy.jpg" alt="Hitesh and Ipsita happy ducks">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><i class="fas fa-camera-retro"></i><span class="font-weight-bold">Maybe the only decent selfie we have</span></div>
+                <img class="img-fluid rounded" src="../assets/images/s1.jpg" alt="Hitesh and Ipsita decent selfie">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><span class="font-italic">When an attempt to click a fake candid converts into a genuine candid...</span><br><br><span class="font-weight-bold"><i class="fas fa-camera-retro"></i>You make me laugh!</span></div>
+                <img class="img-fluid rounded" src="../assets/images/you_make_me_laugh.jpg" alt="You make me laugh">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><span class="font-weight-bold">When he asked </span><span class="font-italic">"Would you like to share a lifetime of memories with me?" </span><br><br><span class="font-weight-bold"><i class="fas fa-camera-retro"></i>And she said yes!</span></div>
+                <img class="img-fluid rounded" src="../assets/images/proposal.jpeg" alt="Proposal">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><i class="fas fa-camera-retro"></i><span class="font-weight-bold">31st August 2018</span> <span class="font-italic">- The day we got engaged!</span></div>
+                <img class="img-fluid rounded" src="../assets/images/3825.jpg" alt="Hitesh and Ipsita get engaged">
+                <img class="img-fluid rounded" src="../assets/images/3955.jpg" alt="Hitesh and Ipsita get engaged">
+                <img class="img-fluid rounded" src="../assets/images/3963.jpg" alt="Hitesh and Ipsita get engaged">
+                <img class="img-fluid rounded" src="../assets/images/4059.jpg" alt="Hitesh and Ipsita get engaged">
+                <!-- <img class="img-fluid rounded" src="../assets/images/4060.jpg" alt="Hitesh and Ipsita get engaged"> -->
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><span class="font-italic">Like a river flows surely to the sea<br>Darling so it goes,<br>Some things are meant to be...<br>Take my hand, take my whole life too</span><br><br><span class="font-weight-bold"><i class="fas fa-camera-retro"></i>For I can't help falling in love with you!</span></div>
+                <img class="img-fluid rounded" src="../assets/images/4060.jpg" alt="Can't help falling in love">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><span class="font-italic">"There is a child inside each one of us, who comes out in front of the person we are most comfortable with."</span><br><br><span class="font-weight-bold"><i class="fas fa-camera-retro"></i>Random quote found on Google</span></div>
+                <img class="img-fluid rounded" src="../assets/images/engagement3.jpg" alt="No points for guessing">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><i class="fas fa-camera-retro"></i><span class="font-italic">No points for guessing who's the crazy one!</span></div>
+                <img class="img-fluid rounded" src="../assets/images/engagement2.jpg" alt="No points for guessing">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><i class="fas fa-camera-retro"></i><span class="font-weight-bold">The one with Misha</span></div>
+                <img class="img-fluid rounded" src="../assets/images/misha.jpg" alt="The one with Misha">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <div class="row memory-card">
+            <div class="col-12">
+                <div class="h6"><i class="fas fa-camera-retro"></i><span class="font-weight-bold">The ones for the wall</span></div>
+                <img class="img-fluid rounded" src="../assets/images/5s.jpg" alt="One for the wall">
+                <img class="img-fluid rounded" src="../assets/images/13s.jpg" alt="One for the wall">
+                <img class="img-fluid rounded" src="../assets/images/9s.jpg" alt="One for the wall">
+                <img class="img-fluid rounded" src="../assets/images/16.jpg" alt="One for the wall">
+                <img class="img-fluid rounded" src="../assets/images/6.jpg" alt="One for the wall">
+                <img class="img-fluid rounded" src="../assets/images/19.jpg" alt="One for the wall">
+            </div>
+        </div>
+
+        <div class="text-center memory-separator">
+            <i class="fas fa-minus"></i>
+        </div>
+
+        <!-- <div class="text-center go-back">
+            <a class="btn btn-outline-primary map-link" href="../">Go Back To Main Page</a>
+        </div> -->
+
+        <div class="row align-items-center relative visit-our-memory-lane-container">
+            <div class="col-4">
+                <img class="img-fluid rounded" src="../assets/images/first_date.jpg" alt="Hitesh and Ipsita candid">
+            </div>
+            <div class="col-4 no-padding-right no-padding-left">
+                <img class="img-fluid rounded" src="../assets/images/13s.jpg" alt="Hitesh and Ipsita candid">
+            </div>
+            <div class="col-4">
+                <img class="img-fluid rounded" src="../assets/images/4060.jpg" alt="Hitesh and Ipsita candid">
+            </div>
+            <div class="opaque"></div>
+            <a href="../" class="btn btn-primary memory-lane-button">Go Back To Main Page</a>
+        </div>
+    </div>
+</body>
+</html>
